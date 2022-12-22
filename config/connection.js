20b08ac7bbc2471fbc1fe33d387a9de28f9dbb8d
Balkrushna Jadhav <bk@better.club>
2022-12-22 12:45:52 +0530

    vercel setting

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,10 @@ dotenv.config();
 const mongoose = require("mongoose");
 
 const mongoUri = process.env.MONGO_URI ?? "mongodb://localhost:27017";
-
+mongoose.set("strictQuery", true);
 const mongoOptions = {
   useNewUrlParser: true,
+
   useUnifiedTopology: true,
   dbName: "wallet-highlevel",
 };
